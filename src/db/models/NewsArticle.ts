5e61610a487927181b6ed29e51dde49d4c46f7c4
Saurav Patil <saurav250998@gmail.com>
2024-07-10 23:24:00 +0530

    Remove unused function from news article

--- src/db/models/NewsArticle.ts
+++ src/db/models/NewsArticle.ts
@@ -1,33 +1,19 @@
 import {Model} from '@nozbe/watermelondb';
-import {field, text} from '@nozbe/watermelondb/decorators';
+import {field} from '@nozbe/watermelondb/decorators';
 
 class NewsArticle extends Model {
   static table = 'news_articles';
 
-  @text('source_id') sourceId;
-  @text('source_name') sourceName;
-  @text('author') author;
-  @text('title') title;
-  @text('description') description;
-  @text('url') url;
-  @text('url_to_image') urlToImage;
-  @text('published_at') publishedAt;
-  @text('content') content;
-
-  getArticle() {
-    return {
-      sourceId: this.sourceId,
-      sourceName: this.sourceName,
-      author: this.author,
-      title: this.title,
-      description: this.description,
-      url: this.url,
-      urlToImage: this.urlToImage,
-      publishedAt: this.publishedAt,
-      content: this.content,
-      id: this.id,
-    };
-  }
+  @field('source_id') sourceId;
+  @field('source_name') sourceName;
+  @field('author') author;
+  @field('title') title;
+  @field('description') description;
+  @field('url') url;
+  @field('url_to_image') urlToImage;
+  @field('published_at') publishedAt;
+  @field('content') content;
+  @field('is_pinned') isPinned;
 }
 
 export default NewsArticle;
