5ae93575907821fb2dd2f31aca9a9b17b97c070f
Saurav Patil <saurav250998@gmail.com>
2024-07-11 23:04:18 +0530

    Add splash screen for android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import RootNavigation from './src/RootNavigation';
 import Colors from './src/common/Colors';
 import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
 import database from './src/db/database';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <DatabaseProvider database={database}>
       <SafeAreaView style={styles.container}>
